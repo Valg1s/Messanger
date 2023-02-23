8cb1c7158157f312887c186628f74cf5dfb94180
Valg1s <antonsegodin@gmail.com>
2023-02-23 21:34:20 +0100

    Was done: -All main function of db was created -Authenticate block also was created -Main paige was created. Need to do: -Test all db.Add some data to the tables -Redact html files -Create styles(css) -Create block of chat`s -Create normal search on main paige

--- Messanger/asgi.py
+++ Messanger/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Messanger project.
+ASGI config for messanger project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Messanger.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messanger.settings')
 
 application = get_asgi_application()
--- Messanger/wsgi.py
+++ Messanger/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Messanger project.
+WSGI config for messanger project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Messanger.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messanger.settings')
 
 application = get_wsgi_application()
